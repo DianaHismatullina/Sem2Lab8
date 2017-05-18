caa90a4af8afff8f42e03ab32b072e6768ddd6c6
Alex <kik9399@mail.ru>
2017-05-19 02:06:42 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Sem2Lab8
+[![Build Status](https://travis-ci.org/akosoj/Sem2Lab8.svg?branch=master)](https://travis-ci.org/akosoj/Sem2Lab8)
---
+++ include/testclass.hpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+	double Real_;
+	double Im_;
+
+public:
+	Complex() : Real_(0), Im_(0) {};
+	Complex(double Real, double Im) : Real_(Real), Im_(Im) {};
+
+	double getReal() { return Real_; }
+	double getIm() { return Im_; }
+	
+	Complex(const Complex &c) {
+		Real_ = c.Real_;
+		Im_ = c.Im_;
+	}
+
+	Complex operator + (const Complex& c2) {
+		Complex tmp(*this);
+		tmp += c2;
+		return tmp;
+	}
+	Complex operator - (const Complex& c2) {
+		Complex tmp(*this);
+		tmp -= c2;
+		return tmp;
+	}
+	Complex& operator /= (const Complex& c2)
+	{
+		double tmp;
+		tmp = (Real_*c2.Real_ + Im_*c2.Im_) / (c2.Real_*c2.Real_ + c2.Im_*c2.Im_);
+		Im_ = (c2.Real_*Im_ - Real_*c2.Im_) / (c2.Real_*c2.Real_ + c2.Im_*c2.Im_);
+		Real_ = tmp;
+		return *this;
+	}
+	Complex& operator *= (const Complex& c2)
+	{
+		double tmp;
+		tmp = Real_*c2.Real_ - Im_*c2.Im_;
+		Im_ = Real_*c2.Im_ + c2.Real_*Im_;
+		Real_ = tmp;
+		return *this;
+	}
+	Complex  operator* (const Complex& c2) {
+		Complex tmp(*this);
+		tmp *= c2;
+		return tmp;
+	}
+	Complex  operator/ (const Complex& c2) {
+		Complex tmp(*this);
+		tmp /= c2;
+		return tmp;
+	}
+	Complex& operator+=(const Complex& c2) {
+		Real_ += c2.Real_;
+		Im_ += c2.Im_;
+		return *this;
+	}
+	Complex& operator-=(const Complex& c2) {
+		Real_ -= c2.Real_;
+		Im_ -= c2.Im_;
+		return *this;
+	}
+	Complex& operator= (const Complex& res) {
+		if (this != &res) {
+			Real_ = res.Real_;
+			Im_ = res.Im_;
+		}
+		return *this;
+	}
+	bool operator== (const Complex& c2) {
+		if ((Real_ == c2.Real_) && (Im_ == c2.Im_)) {
+			return true;
+		}
+		else return false;
+	}
+
+	void print(ostream&) {
+		cout << Real_ << " + i*(" << Im_ << ')' << endl;
+	}
+	friend ostream& operator << (ostream&, const Complex&);
+	friend istream& operator >> (istream&, Complex&);
+};
+ostream & operator<<(ostream & out, Complex &c)
+{
+	c.print(out);
+	return out;
+}
+istream & operator >> (istream & in, Complex &c)
+{
+	in >> c.Real_;
+	in >> c.Im_;
+	return in;
+}
