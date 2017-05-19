e35349a5cffc4e989bce0be9c040e7a5399f2a36
Alex <kik9399@mail.ru>
2017-05-19 11:46:54 +0300

    div0test

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -73,3 +73,9 @@ SCENARIO("operator ==") {
 	Complex c2(3, 1);
 	REQUIRE(c1==c2);
 }
+SCENARIO("operator div0","[mayfail]") {
+	Complex c1(7, 6);
+	Complex c2(7, 0);
+	Complex c3(1, 0);
+	REQUIRE((c1/c2)==c3);
+}
